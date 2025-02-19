f250dda17906008f2f979c1d0d50b745f7091dfc
Pavan Varma <pavanvarma058@gmail.com>
2025-02-19 15:46:36 +0530

    added JS file to Recipe-Book project

---
+++ 03 Recipe-Book/script.js
@@ -0,0 +1,47 @@
+const API_KEY = "214f95901221414aba905b60e4d4aa49";
+const recipeListItem = document.getElementById("recipe-list");
+function displayRecipes(recipes) {
+  recipeListItem.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-item");
+    recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe image";
+
+    recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerText = recipe.title;
+
+    recipeIngredientEl = document.createElement("p");
+    receipeIngredientEl.innerHTML = `
+     <Strong>Ingredients:</strong> ${recipe.extendedIngredients
+       .map((ingredient) => ingredient.original)
+       .join(", ")} 
+        `;
+    recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.innerText = "View Recipe";
+
+    recipeItemEl.appendChild(recipeImageEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientEl);
+    recipeItemEl.appendChild(recipeLinkEl);
+    recipeListItem.appendChild(recipeItemEl);
+  });
+}
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apikey=${API_KEY}`
+  );
+
+  const data = await response.json();
+  return data.recipes;
+}
+
+async function init() {
+  const recipes = await getRecipes();
+  displayRecipes(recipes);
+}
+
+init();
